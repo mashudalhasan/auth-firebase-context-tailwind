f63fba064abc8c6e9c0d62e8259fb2d2656b48cf
mashudalhasan <mashudalhasan554@gmail.com>
2023-04-28 23:41:20 +0600

    implemented login and registration

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="navbar bg-primary text-primary-content">
+        <a className="btn btn-ghost normal-case text-xl">AuthMaster</a>
+        <Link className="btn btn-ghost normal-case text-xl" to="/">
+          Home
+        </Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/login">
+          Login
+        </Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/register">
+          Register
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwbxZ7bQyaR7l6310THtdAbzsvFTcOC5I",
+  authDomain: "auth-firebase-context-ta-5a5cd.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-5a5cd",
+  storageBucket: "auth-firebase-context-ta-5a5cd.appspot.com",
+  messagingSenderId: "599017697669",
+  appId: "1:599017697669:web:97b22d6f4f560d00a3d4ec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
